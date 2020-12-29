6c79b77c7c2faa37317a4032ba9e109e95da339d
Asiri1996 <asirisenith@gmail.com>
2020-12-29 21:40:54 +0530

    Navigating Links in the Page using RouterLink in the angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,31 @@ import { AddUserComponent } from './components/add-user/add-user.component';
 import { FormsModule } from '@angular/forms';
 import { UserService } from './services/user.service';
 import { LogService } from './services/log.service';
+import { HomeComponent } from './components/home/home.component';
+import { PersonComponent } from './components/person/person.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: PersonComponent },
+  { path: 'categories', component: CategoriesComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    AddUserComponent
+    AddUserComponent,
+    HomeComponent,
+    PersonComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     UserService,
